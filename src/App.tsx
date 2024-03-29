8789e4b6dc3dc98ba92c0ae47565fd6accb02b1e
suarez8924 <34613811+suarez8924@users.noreply.github.com>
2024-03-29 13:11:27 +0000

    set project base

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <main className={styles.app}>
+      <div className={styles.attribution}>
+        Challenge by{' '}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </main>
+  );
+}
+
+export default App;
